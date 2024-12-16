e3199026a88315d5285ba6d7270b076b50983557
SamakovskiyVO <146663839+Slavik-satana@users.noreply.github.com>
2024-12-16 21:01:02 +0300

    Update app.py

--- spark/app.py
+++ spark/app.py
@@ -1,5 +1,5 @@
 # Используем базовый образ
-FROM timmyalvice8/pyspark-consumer:latest
+FROM smv/pyspark-consumer:latest
 
 # Копируем файл app.py из текущей директории в директорию app внутри контейнера
 COPY app.py /app/app.py
